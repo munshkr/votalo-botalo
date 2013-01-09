fc2274677c80d3cd710ffcfc2048d82a61a09865
Damián Silvani <munshkr@gmail.com>
2013-01-08 21:25:27 -0300

    Set Unicorn workers to 1

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -7,7 +7,7 @@
 # a more verbose configuration using more features.
 
 listen 2007 # by default Unicorn listens on port 8080
-worker_processes 2 # this should be >= nr_cpus
+worker_processes 1 # this should be >= nr_cpus
 pid "/home/vob/votalo-botalo/shared/pids/unicorn.pid"
 stderr_path "/home/vob/votalo-botalo/shared/log/unicorn.err.log"
 stdout_path "/home/vob/votalo-botalo/shared/log/unicorn.log"
